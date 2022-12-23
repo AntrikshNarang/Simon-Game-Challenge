d60a5da35f1e56b1f43a1c6963e9c61ecc3afae8
AntrikshNarang <itsantriksh@gmail.com>
2022-12-24 00:09:06 +0530

    Added Sounds on click

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 $(".btn").on("click",function(buttonPressed){
     var buttonPressed = this;
+    // console.log(buttonPressed);
     buttonClickAnimation(buttonPressed);
 });
 var arr=[],num;
@@ -12,6 +13,23 @@ $("h1").on("click",function(){
     buttonClickAnimation($($(".btn")[num]));
 });
 function buttonClickAnimation(buttonPressed){
+    var audioVar = $(buttonPressed).attr("id");
+    if(audioVar === "red"){
+        var red = new Audio('sounds/red.mp3');
+        red.play();
+    }
+    if(audioVar === "green"){
+        var green = new Audio('sounds/green.mp3');
+        green.play();
+    }
+    if(audioVar === "yellow"){
+        var yellow = new Audio('sounds/yellow.mp3');
+        yellow.play();
+    }
+    if(audioVar === "blue"){
+        var blue = new Audio('sounds/blue.mp3');
+        blue.play();
+    }
     setTimeout(function(){
         $(buttonPressed).toggleClass("pressed");
     },200);
