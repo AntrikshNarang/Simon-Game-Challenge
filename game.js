3585da44d8fdca606bd15aa32443589f65b5b4ac
AntrikshNarang <itsantriksh@gmail.com>
2022-12-23 23:53:12 +0530

    Added Animations to Buttons

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
-$(".btn").on("click",function(){
-    $(".btn").toggleClass("pressed");
+$(".btn").on("click",function(buttonPressed){
+    var buttonPressed = this;
+    buttonClickAnimation(buttonPressed);
 });
+var arr=[],num;
+$("h1").on("click",function(){
+    num = Math.floor((Math.random()*4));
+    console.log(num);
+    arr.push(num);
+    console.log(arr);
+    // $($(".btn")[num]).toggleClass("pressed");
+    buttonClickAnimation($($(".btn")[num]));
+});
+function buttonClickAnimation(buttonPressed){
+    setTimeout(function(){
+        $(buttonPressed).toggleClass("pressed");
+    },200);
+        $(buttonPressed).toggleClass("pressed");
+}
